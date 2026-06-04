11c1ec8d87d5de4a6d340fd171bd403d5a06fd2a
CHANDAN KUMAR <chandanravi125@gmail.com>
2026-06-04 12:31:18 +0530

    Add CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from .core.database import Base, engine
 from .routes.product_routes import router as product_router
 
@@ -10,7 +10,16 @@ from .routes.order_routes import router as order_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Inventory Management API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://inventory-order-management-sys-fron.vercel.app",
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(product_router)
 app.include_router(customer_router)
 app.include_router(order_router)
